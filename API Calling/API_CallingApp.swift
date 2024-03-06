37fb3b4eed408168cde46efd1033142f24136522
Fabiola Rocha Marquez <frochamarquez@bsd220.org>
2024-03-06 10:00:02 -0600

    Initial Commit

---
+++ API Calling/API_CallingApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_CallingApp.swift
+//  API Calling
+//
+//  Created by Fabiola Rocha Marquez on 3/6/24.
+//
+
+import SwiftUI
+
+@main
+struct API_CallingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
